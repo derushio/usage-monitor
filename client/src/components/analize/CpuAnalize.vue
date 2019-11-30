169447afd27bd70d7f23ee827f94617d3b6d3eac
derushio <derushio@gmail.com>
2019-11-30 18:53:35 +0900

    <master>[new file] add CpuMonitor

---
+++ client/src/components/analize/CpuAnalize.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+.cpu-analize
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CpuAnalize extends Vue {}
+Vue.component('CpuAnalize', CpuAnalize);
+</script>
+
+<style lang='stylus' scoped>
+@require '~@/assets/styles/entry/variable.styl';
+
+.cpu-analize {}
+</style>
---
+++ client/src/components/monitor/CpuMonitor.vue
@@ -0,0 +1,17 @@
+<template lang='pug'>
+.cpu-monitor
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CpuMonitor extends Vue {}
+Vue.component('CpuMonitor', CpuMonitor);
+</script>
+
+<style lang='stylus' scoped>
+@require '~@/assets/styles/entry/variable.styl';
+
+.cpu-monitor {}
+</style>
